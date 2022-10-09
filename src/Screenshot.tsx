f5921b8d936914e4e7472b64d8653e18c3037749
Randy Asuncion <ranasunc@gmail.com>
2022-10-09 09:12:32 -0700

    Make Screenshot component dynamic

--- src/Screenshot.tsx
+++ src/Screenshot.tsx
@@ -1,21 +1,23 @@
 interface Props {
+	title: string;
 	imgUrl: string;
 }
 
-const Screenshot: React.FC<Props> = ({ imgUrl }) => {
+const Screenshot: React.FC<Props> = ({ title, imgUrl }) => {
+	console.log(imgUrl);
 	return (
-		<div className="text-xs rounded-lg bg-clip-padding overflow-hidden shadow-md">
+		<div className="text-xs rounded-lg bg-clip-padding overflow-hidden shadow-md border-gray-200 dark:border-gray-800 border mx-auto md:w-full">
 			<div className=" bg-slate-700 p-2 flex text-gray-400 font-bold mono items-center">
 				<div className="flex w-1/3 gap-2">
 					<div className="h-3 w-3 bg-red-400 rounded-full"></div>
 					<div className="h-3 w-3 bg-yellow-400 rounded-full"></div>
 					<div className="h-3 w-3 bg-green-400 rounded-full"></div>
 				</div>
-				<div className="w-1/3 text-center">Js Notebook</div>
+				<div className="w-1/3 text-center">{title}</div>
 			</div>
-			<div
-				className={`bg-cover bg-[url('${imgUrl}')] h-[220px] md:h-[180px]`}
-			></div>
+			<div className="bg-cover h-[250px] md:h-[180px]">
+				<img src={imgUrl} alt="" />
+			</div>
 		</div>
 	);
 };
