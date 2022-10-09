203a923bf8619cc38f239614c7ea87514dd7688a
Randy Asuncion <ranasunc@gmail.com>
2022-10-09 06:04:05 -0700

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Page from './Page';
 import Initial from './Initial';
 import Mode from './Mode';
@@ -9,6 +9,22 @@ import { Card, CardFooter, CardText, CardTitle } from './Card';
 
 function App() {
 	const [dark, setDark] = useState(false);
+	const [yoe, setYoe] = useState(new Date().getFullYear() - 2013);
+	const [now, setNow] = useState(0);
+	const [showMore, setShowMore] = useState(false);
+
+	useEffect(() => {
+		let secTimer = setInterval(() => {
+			const date1 = new Date();
+			const date2 = new Date('2013-09-01 00:00:00');
+			const seconds = Math.round(
+				(date1.getTime() - date2.getTime()) / 1000
+			);
+			setNow(seconds);
+		}, 1000);
+
+		return () => clearInterval(secTimer);
+	}, []);
 
 	return (
 		<Page darkMode={dark}>
@@ -22,7 +38,7 @@ function App() {
 				{/* INTRO */}
 				<section className="py-[120px]">
 					<div>
-						<h1 className="text-4xl leading-[48px]">
+						<h1 className="text-4xl md:leading-[48px]">
 							Hello! 👋 I'm <strong>Randy.</strong>
 							<br />I build modern web applications.
 						</h1>
@@ -32,19 +48,111 @@ function App() {
 				{/* ABOUT */}
 				<section className="py-10">
 					<h2 className="text-3xl">About</h2>
-					<div className="flex flex-col-reverse gap-4 justify-between items-center mt-4 md:flex-row">
-						<div className="w-full text-center md:w-2/3 md:text-left ">
-							<p className="text-grey-darkest text-lg leading-normal">
+					<div className="text-grey-darkest text-lg leading-normal flex flex-col-reverse gap-4 justify-between items-start mt-4 md:flex-row">
+						<div className="w-full md:w-2/3 ">
+							<p className=" ">
 								I'm a software engineer specializing in web
 								development. I'm based in Los Angeles, CA and
 								currently work as a full stack developer for
 								Water Source Solutions.
 							</p>
+							<p className="mt-4 ">
+								I started as a self-taught developer and have
+								been building web applications professionaly for
+								more than {yoe} years now or exactly {now}{' '}
+								seconds.
+							</p>
 							<div className="mt-4">
-								<Anchor url="">Download Resume &darr;</Anchor>
+								{!showMore ? (
+									<span
+										className=" text-gray-400 cursor-pointer hover:underline text-sm font-thin"
+										onClick={() => setShowMore(true)}
+									>
+										&darr; SHOW MORE
+									</span>
+								) : (
+									<div className="">
+										<h3 className="mt-8 text-2xl">
+											Experience
+										</h3>
+										<div>
+											<h4 className="text-lg mt-2">
+												Full Stack Developer
+											</h4>
+											<h5 className="uppercase mono text-sm">
+												Water Source Solutions{' '}
+												<span>
+													2013 &mdash; Present
+												</span>
+											</h5>
+										</div>
+										<div>
+											<h4 className="text-lg mt-2">
+												Front End Developer
+											</h4>
+											<h5 className="uppercase mono text-sm">
+												CRS Consulting 2016 &mdash; 2017
+											</h5>
+										</div>
+										<h3 className="mt-8 text-2xl">
+											Education
+										</h3>
+										<div>
+											<h4 className="text-lg mt-2">
+												BS Computer Science
+											</h4>
+											<h5 className="uppercase mono text-sm">
+												Western Governors University
+												2021 &mdash; 2022
+											</h5>
+										</div>
+										<div>
+											<h4 className="text-lg mt-2">
+												Associates in Computer
+												Technology
+											</h4>
+											<h5 className="uppercase mono text-sm">
+												Systems Technology Institute{' '}
+												1999 &mdash; 2021
+											</h5>
+										</div>
+										<h3 className="mt-8 text-2xl">
+											Certifications
+										</h3>
+										<div>
+											<h4 className="text-lg mt-2">
+												ITIL Foundation Certificate in
+												IT Service Management
+											</h4>
+											<h5 className="uppercase mono text-sm">
+												AXELOS Global Best Practice
+												&mdash; 2022
+											</h5>
+										</div>
+										<div>
+											<h4 className="text-lg mt-2">
+												CompTIA Project+ Certification
+											</h4>
+											<h5 className="uppercase mono text-sm">
+												CompTIA &mdash; 2022
+											</h5>
+										</div>
+										<div className="mt-8">
+											<Anchor url="">
+												Download Resume &rarr;
+											</Anchor>
+										</div>
+										<span
+											className=" text-gray-400 cursor-pointer hover:underline mt-8 text-sm font-thin inline-block"
+											onClick={() => setShowMore(false)}
+										>
+											&uarr; SHOW LESS
+										</span>
+									</div>
+								)}
 							</div>
 						</div>
-						<div className="h-[150px] w-[150px] md:h-[200px] md:w-[200px] bg-gray-200 rounded-full"></div>
+						<div className="h-[80px] w-[80px] md:h-[150px] md:w-[150px] bg-gray-200 rounded-full"></div>
 					</div>
 				</section>
 
@@ -56,58 +164,63 @@ function App() {
 					</p>
 					<Card>
 						<CardTitle>
-							<Initial>jn</Initial>
-							<h3 className="ml-4 text-lg font-bold">
-								JS Notebook
-							</h3>
+							<h3 className="text-lg font-bold">JS Notebook</h3>
 						</CardTitle>
 						<CardText>
 							A jupyter notebook clone for javascript.
+							<div className="flex flex-wrap gap-1 mt-4">
+								<Tag>Typescript</Tag>
+								<Tag>React</Tag>
+								<Tag>Redux-Toolkit</Tag>
+								<Tag>Tailwindcss</Tag>
+								<Tag>Cypress</Tag>
+							</div>
 						</CardText>
 						<CardFooter>
 							<div className="">
 								<Anchor url="">View website &rarr;</Anchor>
 							</div>
-							<div className="flex gap-1">
-								<Tag>React</Tag>
-								<Tag>Typescript</Tag>
-							</div>
 						</CardFooter>
 					</Card>
 					<Card>
 						<CardTitle>
-							<Initial>hy</Initial>
-							<h3 className="ml-4 text-lg font-bold">Heartly</h3>
+							<h3 className="text-lg font-bold">Heartly</h3>
 						</CardTitle>
 						<CardText>
-							A web application that calculates a person's risk
-							factor for heart disease using machine learning.
+							Calculates a person's risk factor for heart disease.
+							<div className="flex flex-wrap gap-1 mt-4">
+								<Tag>Vue.js</Tag>
+								<Tag>Python</Tag>
+								<Tag>Flask</Tag>
+								<Tag>Vuetify</Tag>
+								<Tag>Machine Learning</Tag>
+								<Tag>REST API</Tag>
+							</div>
 						</CardText>
 						<CardFooter>
 							<div className="">
 								<Anchor url="">View website &rarr;</Anchor>
 							</div>
-							<div className="flex gap-1">
-								<Tag>React</Tag>
-								<Tag>Typescript</Tag>
-							</div>
 						</CardFooter>
 					</Card>
 					<Card>
 						<CardTitle>
-							<Initial>ig</Initial>
-							<h3 className="ml-4 text-lg font-bold">
+							<h3 className="text-lg font-bold">
 								Invoice Generator
 							</h3>
 						</CardTitle>
-						<CardText>A simple invoice generator.</CardText>
+						<CardText>
+							Create invoices and download them as pdf.
+							<div className="flex flex-wrap gap-1 mt-4">
+								<Tag>Vue.js</Tag>
+								<Tag>Tailwindcss</Tag>
+								<Tag>Pdfmake</Tag>
+								<Tag>PWA</Tag>
+							</div>
+						</CardText>
 						<CardFooter>
 							<div className="">
 								<Anchor url="">View website &rarr;</Anchor>
-							</div>
-							<div className="flex gap-1">
-								<Tag>React</Tag>
-								<Tag>Typescript</Tag>
 							</div>
 						</CardFooter>
 					</Card>
@@ -125,7 +238,7 @@ function App() {
 					<div className="flex justify-center flex-col-reverse md:flex-row md:justify-between items-center gap-4">
 						<span className="text-xs text-center">
 							This site was built from scratch using <u>React</u>,{' '}
-							<u>TaildwindCSS</u> & <u>Iconoir</u>.
+							<u>Taildwindcss</u> & <u>Iconoir</u>.
 						</span>
 						<span className="flex gap-2">
 							<Initial>
