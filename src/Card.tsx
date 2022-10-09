38b3e96ca707cd203a6f7edfa6931a79385b34cd
Randy Asuncion <ranasunc@gmail.com>
2022-10-09 06:03:52 -0700

    Add screenshot component to card

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import Screenshot from './Screenshot';
 
 interface Props {
 	children: ReactNode;
@@ -7,25 +8,29 @@ interface Props {
 export const Card: React.FC<Props> = ({ children }) => {
 	return (
 		<div className="w-full my-4 flex gap-4 items-center flex-col md:flex-row py-4">
-			<div className="w-full md:w-[200px]">
-				<div className="h-[200px] w-full md:w-[200px] bg-gray-200"></div>
+			<div className="w-full md:w-2/5 h-full">
+				<Screenshot
+					imgUrl={
+						'https://raw.githubusercontent.com/ranasun/js-notebook/main/screenshot.png'
+					}
+				/>
 			</div>
-			<div className="w-full">{children}</div>
+			<div className="w-full md:w-3/5">{children}</div>
 		</div>
 	);
 };
 
 export const CardTitle: React.FC<Props> = ({ children }) => {
-	return <div className="flex items-center ">{children}</div>;
+	return <div className="">{children}</div>;
 };
 
 export const CardText: React.FC<Props> = ({ children }) => {
-	return <p className="mt-5 text-lg">{children}</p>;
+	return <p className="text-lg">{children}</p>;
 };
 
 export const CardFooter: React.FC<Props> = ({ children }) => {
 	return (
-		<div className="mt-8 flex gap-2 items-start flex-col-reverse justify-between md:flex-row md:items-end">
+		<div className="mt-4 flex gap-2 items-start flex-col-reverse justify-between md:flex-row md:items-end">
 			{children}
 		</div>
 	);
