6096b7bcbb5a1e40d1dd1f6d48b780588654d699
Randy Asuncion <ranasunc@gmail.com>
2022-10-10 14:06:57 -0700

    Set curor to default

--- src/Tag.tsx
+++ src/Tag.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 const Tag: React.FC<Props> = ({ children }) => {
 	return (
-		<span className="bg-gray-100 text-gray-500 dark:bg-gray-800 dark:text-gray-500 py-1 px-3 inline-block rounded-full text-xs whitespace-nowrap">
+		<span className="bg-gray-100 text-gray-500 dark:bg-gray-800 dark:text-gray-500 py-1 px-3 inline-block rounded-full text-xs whitespace-nowrap cursor-default">
 			{children}
 		</span>
 	);
