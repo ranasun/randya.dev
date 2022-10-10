ff3dd1075cba5aad5fb9e0c40f88627564e7fa74
Randy Asuncion <ranasunc@gmail.com>
2022-10-10 09:19:42 -0700

    Add mailto link

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -2,7 +2,15 @@ const Contact = () => {
 	return (
 		<section className=" py-40">
 			<h1 className="text-3xl text-center">
-				You've reach the end. Don't forget to <u>say hello</u>!
+				You've reach the end. Don't forget to{' '}
+				<a
+					href="mailto:randya.dev@gmail.com"
+					target="__blank"
+					className="hover:underline font-bold"
+				>
+					say hello
+				</a>
+				!
 			</h1>
 		</section>
 	);
