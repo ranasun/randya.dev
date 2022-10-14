41c63e9fe258f446f8f6e899b48da117cd545d7e
Randy Asuncion <ranasunc@gmail.com>
2022-10-14 03:23:04 -0700

    Add hand animation

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -3,7 +3,11 @@ const Intro = () => {
 		<section className="py-[120px]">
 			<div>
 				<h1 className="text-4xl md:leading-[48px]">
-					Hello! 👋 I'm <strong>Randy.</strong>
+					Hello!{' '}
+					<span className="animate-hand inline-block origin-bottom ml-1">
+						🖐🏼
+					</span>{' '}
+					I'm <strong>Randy.</strong>
 					<br />I build modern web applications.
 				</h1>
 			</div>
