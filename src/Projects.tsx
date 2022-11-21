59d280fbc8d28d8bc6fd89fd9767e6f461c420b7
Randy Asuncion <ranasunc@gmail.com>
2022-11-21 00:22:52 -0800

    revise heading

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
 		<section className="py-10">
 			<h2 className="text-3xl font-bold">Projects</h2>
 			<p className="w-full md:w-2/3 mt-4 text-grey-darkest text-lg leading-normal">
-				Here are some of the projects I've built recently.
+				Here are some of the personal projects I've built recently.
 			</p>
 			{projects.map((props) => (
 				<Project key={props.title} {...props} />
